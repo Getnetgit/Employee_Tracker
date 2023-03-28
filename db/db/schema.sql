c49d9044260790258a7adcd0b2d275d797635b27
Get Ali <getnet4fafi@gmail.com>
2023-03-27 21:19:11 -0700

    command line front end

--- db/db/schema.sql
+++ db/db/schema.sql
@@ -10,7 +10,7 @@ CREATE TABLE department (
 
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30),
+    title VARCHAR(30) NOT NULL,
     salary DECIMAL,
     department_id INT,
     FOREIGN KEY (department_id)
@@ -20,8 +20,8 @@ CREATE TABLE role (
 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
     role_id INT,
     FOREIGN KEY (role_id)
     REFERENCES role(id)
